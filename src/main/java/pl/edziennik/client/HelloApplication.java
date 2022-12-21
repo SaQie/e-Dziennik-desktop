e910df4d56d038889b009c809629d06f2d3f8d71
SaQie <Kamcio99@interia.com>
2022-12-21 22:20:25 +0100

    add student-list-view

--- src/main/java/pl/edziennik/client/HelloApplication.java
+++ src/main/java/pl/edziennik/client/HelloApplication.java
@@ -3,16 +3,18 @@ package pl.edziennik.client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("authorization-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 650, 450);
+        Scene scene = new Scene(fxmlLoader.load(), 950, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/pl/edziennik/client/HelloController.java
+++ src/main/java/pl/edziennik/client/HelloController.java
@@ -1,11 +1,14 @@
 package pl.edziennik.client;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -14,16 +17,31 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
 
+    @FXML
+    private TableColumn<String, String> firstColumn;
 
     @FXML
-    private ComboBox<String> accountType;
+    private TableColumn<String, String> fourthColumn;
 
     @FXML
-    private AnchorPane anchor;
+    private TableColumn<String, String> secondColumn;
+
+    @FXML
+    private TableView<String> testTable;
+
+    @FXML
+    private TableColumn<String, String> thirdColumn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<String> strings = FXCollections.observableList(List.of("Nauczyciel", "Student"));
-        accountType.getItems().addAll(strings);
+//        firstColumn.setCellValueFactory(c -> new SimpleStringProperty("Dupa"));
+//
+//        secondColumn.setCellValueFactory(c -> new SimpleStringProperty("Nazwisko"));
+//        thirdColumn.setCellValueFactory(c -> new SimpleStringProperty("Pesel"));
+//        fourthColumn.setCellValueFactory(c -> new SimpleStringProperty("Adres"));
+//
+//        ObservableList<String> strings = FXCollections.observableList(List.of("asd", "qwe", "erer", "dvdv"));
+//        testTable.setItems(strings);
+
     }
 }

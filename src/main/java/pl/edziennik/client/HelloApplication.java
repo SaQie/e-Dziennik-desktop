8970e219b389b3e093b53434b879bc8592978271
SaQie <Kamcio99@interia.com>
2022-12-21 19:36:15 +0100

    minor changes in authorization view css

--- src/main/java/pl/edziennik/client/HelloApplication.java
+++ src/main/java/pl/edziennik/client/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,7 +14,6 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("authorization-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 450);
         stage.setTitle("Hello!");
-
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/pl/edziennik/client/HelloController.java
+++ src/main/java/pl/edziennik/client/HelloController.java
@@ -1,14 +1,29 @@
 package pl.edziennik.client;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox<String> accountType;
+
+    @FXML
+    private AnchorPane anchor;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> strings = FXCollections.observableList(List.of("Nauczyciel", "Student"));
+        accountType.getItems().addAll(strings);
     }
 }
